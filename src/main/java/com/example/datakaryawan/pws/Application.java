827f1ede4a521b3020e9f66e5ad1ff5367a9771f
annisaysfn <63139331+annisaysfn@users.noreply.github.com>
2022-01-12 07:30:27 +0700

    updates

---
+++ src/main/java/com/example/datakaryawan/pws/Application.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.datakaryawan.pws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
